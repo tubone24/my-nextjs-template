fdd263e72588134241846eed9eecc9ac1bafd0df
tubone24 <tubo.yyyuuu@gmail.com>
2021-05-07 00:27:41 +0900

    StoryBook Tepl

---
+++ .storybook/main.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+module.exports = {
+  addons: ['@storybook/preset-scss'],
+};
--- src/components/footer.stories.tsx
+++ src/components/footer.stories.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { Box, Icon, Image } from '@chakra-ui/react';
 
-import Footer from '../../src/components/footer'
+import Footer from './footer'
 
 export default {
   title: 'components/Footer',
   component: Footer,
 } as Meta;
 
-const Template: Story = (args) => (
+export const Template: Story = (args) => (
   <Footer />
 );
