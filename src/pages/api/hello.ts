5ce215b51ea13c0218bd67d33cf09f2b1058df3c
tubone24 <tubo.yyyuuu@gmail.com>
2021-05-12 08:15:17 +0900

    :bug: Fix Bug gridList.tsx

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+interface Response {
+  year: number
+  month: number
+  date: number
+  hours: number
+  minutes: number
+  seconds: number
+
+}
+
 const handler = (req: NextApiRequest, res: NextApiResponse): void => {
   const currentDate = new Date();
-  const datetime = `${currentDate.getFullYear()}/${(currentDate.getMonth()+1 < 10)?"0":""}${currentDate.getMonth() + 1}/${(currentDate.getDate() < 10)?"0":""}${currentDate.getDate()} @ ${(currentDate.getHours() < 10)?"0":""}${currentDate.getHours()}:${(currentDate.getMinutes() < 10)?"0":""}${currentDate.getMinutes()}:${(currentDate.getSeconds() < 10)?"0":""}${currentDate.getSeconds()}`
+  // const datetime = `${currentDate.getFullYear()}/${(currentDate.getMonth()+1 < 10)?"0":""}${currentDate.getMonth() + 1}/${(currentDate.getDate() < 10)?"0":""}${currentDate.getDate()} @ ${(currentDate.getHours() < 10)?"0":""}${currentDate.getHours()}:${(currentDate.getMinutes() < 10)?"0":""}${currentDate.getMinutes()}:${(currentDate.getSeconds() < 10)?"0":""}${currentDate.getSeconds()}`
+  const datetime = currentDate.getFullYear()
   res.statusCode = 200
   res.json(datetime)
 }
