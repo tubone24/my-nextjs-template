a3b19cfa250b7643b49dc096b389ad8b4b1fec99
tubone24 <tubo.yyyuuu@gmail.com>
2021-05-14 23:14:51 +0900

    WIP: テストでエラーがでる。

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { render } from 'test/testUtils'
 import HomePage from 'src/pages'
 import { RecoilRoot } from 'recoil'
-import {ChakraProvider} from "@chakra-ui/react";
-
-global.fetch = require('node-fetch');
 
 test('index snapshot', () => {
   const { container } = render(
