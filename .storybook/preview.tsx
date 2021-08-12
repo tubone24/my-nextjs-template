ebda4ac9466591bc92c1f05a2b6803b72a831a8a
tubone24 <tubo.yyyuuu@gmail.com>
2021-08-13 00:26:53 +0900

    WIP: StorybookのChakraUIがCORSになって動かない
    https://chakra-ui.netlify.app/stories.json?version=1.6.6

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,15 @@
-import * as React from 'react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { StoryContext } from '@storybook/react';
+import {
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { StoryContext } from '@storybook/react'
+import * as React from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { withPerformance } from 'storybook-addon-performance'
 
 /**
  * Add global context for RTL-LTR switching
@@ -15,18 +24,41 @@ export const globalTypes = {
       items: ['LTR', 'RTL'],
     },
   },
-};
+}
+
+const ColorModeToggleBar = () => {
+  const { toggleColorMode } = useColorMode()
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
+  const nextMode = useColorModeValue('dark', 'light')
+
+  return (
+    <Flex justify="flex-end" mb={4}>
+      <IconButton
+        size="md"
+        fontSize="lg"
+        aria-label={`Switch to ${nextMode} mode`}
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+      />
+    </Flex>
+  )
+}
 
 const withChakra = (StoryFn: Function, context: StoryContext) => {
-  const { direction } = context.globals;
-  const dir = direction.toLowerCase();
+  const { direction } = context.globals
+  const dir = direction.toLowerCase()
+
   return (
     <ChakraProvider theme={extendTheme({ direction: dir })}>
       <div dir={dir} id="story-wrapper" style={{ minHeight: '100vh' }}>
+        <ColorModeToggleBar />
         <StoryFn />
       </div>
     </ChakraProvider>
-  );
-};
+  )
+}
 
-export const decorators = [withChakra];
+export const decorators = [withChakra, withPerformance]
--- src/components/footer.stories.tsx
+++ src/components/footer.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Box, Icon, Image } from '@chakra-ui/react'
 
 import Footer from './footer'
 
