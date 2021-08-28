db501de5ada81ca5caea3a99c8d28bf8ef251ea0
tubone24 <tubo.yyyuuu@gmail.com>
2021-08-28 20:44:02 +0900

    nextjs sentry

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { withSentry } from '@sentry/nextjs'
 
 export interface DateResponse {
   year: string
@@ -10,18 +11,18 @@ export interface DateResponse {
 }
 
 const handler = (req: NextApiRequest, res: NextApiResponse): void => {
-  const currentDate = new Date();
+  const currentDate = new Date()
   // const datetime = `${currentDate.getFullYear()}/${(currentDate.getMonth()+1 < 10)?"0":""}${currentDate.getMonth() + 1}/${(currentDate.getDate() < 10)?"0":""}${currentDate.getDate()} @ ${(currentDate.getHours() < 10)?"0":""}${currentDate.getHours()}:${(currentDate.getMinutes() < 10)?"0":""}${currentDate.getMinutes()}:${(currentDate.getSeconds() < 10)?"0":""}${currentDate.getSeconds()}`
   const datetime: DateResponse = {
     year: `${currentDate.getFullYear()}`,
-    month: `${(currentDate.getMonth()+1 < 10)?"0":""}${currentDate.getMonth() + 1}`,
-    date: `${(currentDate.getDate() < 10)?"0":""}${currentDate.getDate()}`,
-    hours: `${(currentDate.getHours() < 10)?"0":""}${currentDate.getHours()}`,
-    minutes: `${(currentDate.getMinutes() < 10)?"0":""}${currentDate.getMinutes()}`,
-    seconds: `${(currentDate.getSeconds() < 10)?"0":""}${currentDate.getSeconds()}`
-    }
+    month: `${currentDate.getMonth() + 1 < 10 ? '0' : ''}${currentDate.getMonth() + 1}`,
+    date: `${currentDate.getDate() < 10 ? '0' : ''}${currentDate.getDate()}`,
+    hours: `${currentDate.getHours() < 10 ? '0' : ''}${currentDate.getHours()}`,
+    minutes: `${currentDate.getMinutes() < 10 ? '0' : ''}${currentDate.getMinutes()}`,
+    seconds: `${currentDate.getSeconds() < 10 ? '0' : ''}${currentDate.getSeconds()}`,
+  }
   res.statusCode = 200
   res.json(datetime)
 }
 
-export default handler
+export default withSentry(handler)
